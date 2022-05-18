e8b9e14adce7be7e00851b87639167fba705465b
Konrad Ponichtera <konpon96@gmail.com>
2022-05-18 20:45:17 +0200

    Remembering database name for use with Mongo client

--- pkg/stock/mongo/client.go
+++ pkg/stock/mongo/client.go
@@ -4,10 +4,12 @@ import mongoDriver "go.mongodb.org/mongo-driver/mongo"
 import mongoUtils "shopping-cart/pkg/utils/mongo"
 
 var (
-	client mongoDriver.Client
+	client   mongoDriver.Client
+	database string
 )
 
 func Connect(options *mongoUtils.Config) mongoDriver.Client {
 	client = mongoUtils.Connect(options)
+	database = options.Database
 	return client
 }
