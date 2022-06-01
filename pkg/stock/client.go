8f032f7e2d3cf63e5fc0da634e08d15da89e5dc9
moudemans <m.w.m.oudemans@student.tudelft.nl>
2022-06-01 14:47:52 +0200

    Added endpoints for payment ms

--- pkg/stock/client.go
+++ pkg/stock/client.go
@@ -1,10 +1,12 @@
 package stock
 
 import (
+	"context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	stockApi "shopping-cart/api/proto/stock"
+	"time"
 )
 
 var (
@@ -19,4 +21,33 @@ func Connect(address string) *grpc.ClientConn {
 
 	client = stockApi.NewStockClient(conn)
 	return conn
+}
+
+func createContext() (context.Context, context.CancelFunc) {
+	return context.WithTimeout(context.Background(), time.Second*10)
+
+}
+
+func Find(request *stockApi.FindRequest) (*stockApi.FindResponse, error) {
+	ctx, cel := createContext()
+	defer cel()
+	return client.Find(ctx, request)
+}
+
+func Subtract(request *stockApi.SubtractRequest) (*stockApi.EmptyMessage, error) {
+	ctx, cel := createContext()
+	defer cel()
+	return client.Subtract(ctx, request)
+}
+
+func Add(request *stockApi.AddRequest) (*stockApi.EmptyMessage, error) {
+	ctx, cel := createContext()
+	defer cel()
+	return client.Add(ctx, request)
+}
+
+func Create(request *stockApi.CreateRequest) (*stockApi.CreateResponse, error) {
+	ctx, cel := createContext()
+	defer cel()
+	return client.Create(ctx, request)
 }
