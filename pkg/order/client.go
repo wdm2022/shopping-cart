290d5d96b6ce889d9f57f76f337d628cb0c4ce40
moudemans <m.w.m.oudemans@student.tudelft.nl>
2022-06-01 14:30:28 +0200

    Added endpoints for order

--- pkg/order/client.go
+++ pkg/order/client.go
@@ -1,10 +1,12 @@
 package order
 
 import (
+	"context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	orderApi "shopping-cart/api/proto/order"
+	"time"
 )
 
 var (
@@ -19,4 +21,60 @@ func Connect(address string) *grpc.ClientConn {
 
 	client = orderApi.NewOrderClient(conn)
 	return conn
+}
+
+func CreateOrder(userId string) (*orderApi.CreateOrderResponse, error) {
+	ctx, cel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cel()
+	dto := orderApi.CreateOrderRequest{
+		UserId: userId,
+	}
+	return client.CreateOrder(ctx, &dto)
+}
+
+func RemoveOrder(orderId string) (*orderApi.EmptyMessage, error) {
+	ctx, cel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cel()
+	dto := orderApi.RemoveOrderRequest{
+		OrderId: orderId,
+	}
+	return client.RemoveOrder(ctx, &dto)
+}
+
+func GetOrder(orderId string) (*orderApi.GetOrderResponse, error) {
+	ctx, cel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cel()
+	dto := orderApi.GetOrderRequest{
+		OrderId: orderId,
+	}
+	return client.GetOrder(ctx, &dto)
+}
+
+func AddItem(orderId string, itemId string) (*orderApi.EmptyMessage, error) {
+	ctx, cel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cel()
+	dto := orderApi.AddItemRequest{
+		OrderId: orderId,
+		ItemId:  itemId,
+	}
+	return client.AddItem(ctx, &dto)
+}
+
+func RemoveItem(orderId string, itemId string) (*orderApi.EmptyMessage, error) {
+	ctx, cel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cel()
+	dto := orderApi.RemoveItemRequest{
+		OrderId: orderId,
+		ItemId:  itemId,
+	}
+	return client.RemoveItem(ctx, &dto)
+}
+
+func Checkout(orderId string) (*orderApi.EmptyMessage, error) {
+	ctx, cel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cel()
+	dto := orderApi.CheckoutRequest{
+		OrderId: orderId,
+	}
+	return client.Checkout(ctx, &dto)
 }
