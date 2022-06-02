9e0ceaab5b0c9f5df4665d386377b1e925d87b4c
moudemans <melchior.oudemans@gmail.com>
2022-06-02 14:33:22 +0200

    adds function calls to ms from api

--- pkg/api/handlers/order.go
+++ pkg/api/handlers/order.go
@@ -2,83 +2,138 @@ package handlers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	orderApi "shopping-cart/api/proto/order"
+	"shopping-cart/pkg/order"
 )
 
+// TODO: This is not a function belonging to the Order, it should be in payment
 func CreateUser(c *fiber.Ctx) error {
+	userId := c.Params("userId")
 
-	userId, err := c.ParamsInt("userId")
+	// Invalid id / default value returned by c.params
+	if userId == "" {
+		return c.SendStatus(400)
+	}
+
+	response, err := order.CreateOrder(&orderApi.CreateOrderRequest{UserId: userId})
+
 	if err != nil {
-		return err
+		return c.SendStatus(400)
 	}
 
 	return c.JSON(fiber.Map{
-		"userId": userId,
+		"userId": response.OrderId,
 	})
 }
 
 func GetOrder(c *fiber.Ctx) error {
-	orderId, err := c.ParamsInt("orderId")
+	orderId := c.Params("orderId")
+
+	// Invalid id / default value returned by c.params
+	if orderId == "" {
+		return c.SendStatus(400)
+	}
+
+	response, err := order.GetOrder(&orderApi.GetOrderRequest{OrderId: orderId})
+
+	if err != nil {
+		return c.SendStatus(400)
+	}
+
+	err = c.SendStatus(200)
 	if err != nil {
 		return err
 	}
 
 	return c.JSON(fiber.Map{
-		"orderId": orderId,
+		"orderId":   response.OrderId,
+		"paid":      response.Paid,
+		"items":     response.ItemIds,
+		"userId":    response.UserId,
+		"totalCost": response.TotalCost,
 	})
 }
 
 func AddItem(c *fiber.Ctx) error {
+	orderId := c.Params("orderId")
+	// Invalid id / default value returned by c.params
+	if orderId == "" {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
 
-	orderId, err := c.ParamsInt("orderId")
-	if err != nil {
-		return err
+	itemId := c.Params("itemId")
+	// Invalid id / default value returned by c.params
+	if itemId == "" {
+		return c.SendStatus(fiber.StatusBadRequest)
 	}
-	itemId, err := c.ParamsInt("itemId")
+
+	_, err := order.AddItem(&orderApi.AddItemRequest{OrderId: orderId, ItemId: itemId})
+
 	if err != nil {
-		return err
+		return c.SendStatus(fiber.StatusBadRequest)
 	}
-	return c.JSON(fiber.Map{
-		"orderId": orderId,
-		"itemId":  itemId,
-	})
+
+	return c.SendStatus(fiber.StatusOK)
 }
 
 func DeleteItem(c *fiber.Ctx) error {
-
-	orderId, err := c.ParamsInt("orderId")
-	if err != nil {
-		return err
-	}
-	itemId, err := c.ParamsInt("itemId")
-	if err != nil {
-		return err
+	orderId := c.Params("orderId")
+	// Invalid id / default value returned by c.params
+	if orderId == "" {
+		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
-	return c.JSON(fiber.Map{
-		"orderId": orderId,
-		"itemId":  itemId,
-	})
+	itemId := c.Params("itemId")
+	// Invalid id / default value returned by c.params
+	if itemId == "" {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	_, err := order.RemoveItem(&orderApi.RemoveItemRequest{OrderId: orderId, ItemId: itemId})
+
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	return c.SendStatus(fiber.StatusOK)
 }
 
 func DeleteOrder(c *fiber.Ctx) error {
+	orderId := c.Params("orderId")
 
-	userId, err := c.ParamsInt("userId")
-	if err != nil {
-		return err
+	// Invalid id / default value returned by c.params
+	if orderId == "" {
+		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
-	return c.JSON(fiber.Map{
-		"userId": userId,
-	})
+	_, err := order.RemoveOrder(&orderApi.RemoveOrderRequest{OrderId: orderId})
+
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	return c.SendStatus(fiber.StatusOK)
 }
 
 func CreateOrder(c *fiber.Ctx) error {
-	userId, err := c.ParamsInt("orderId")
+	userId := c.Params("userId")
+
+	// Invalid id / default value returned by c.params
+	if userId == "" {
+		return c.SendStatus(400)
+	}
+
+	response, err := order.CreateOrder(&orderApi.CreateOrderRequest{UserId: userId})
+
+	if err != nil {
+		return c.SendStatus(400)
+	}
+
+	err = c.SendStatus(200)
 	if err != nil {
 		return err
 	}
-
 	return c.JSON(fiber.Map{
-		"orderId": userId,
+		"orderId": response.OrderId,
 	})
 }
